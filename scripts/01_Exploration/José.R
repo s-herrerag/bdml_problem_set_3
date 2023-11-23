77a1ffc3ad0b967fb4b78e449aa5d2e4b136c097
JoseDGonzalezR <jd.gonzalezr12@uniandes.edu.co>
2023-11-22 19:23:22 -0500

    Corrección ID, Orden

--- scripts/01_Exploration/José.R
+++ scripts/01_Exploration/José.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6da58e3bc8bb018b517de9ffd5d05b4af671a8c346cf5ea9bd484e71860850da
-size 1951
+oid sha256:1880b81b3beb9b6b3b6f8d1fe0a10c4f9452e099705315a0dc3264170f112447
+size 1962
