548219671c1ceec423f921be857bbfd7e036affc
Santiago Herrera Garcia <sherreragarcia@c40.org>
2023-11-22 20:06:47 -0500

    Unir variables Hector, Jose, Santiago

---
+++ scripts/01_Exploration/vars_exploration_join.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36e356300918ab37860290701cc82dd6675f692d9963e490b3282ab968712b8a
+size 1350
--- scripts/01_Exploration/vars_exploration_pack3.R
+++ scripts/01_Exploration/vars_exploration_pack3.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77b55b0479c88514c27066c1ccdae428c6190d5f0475d9d6cbf8d8083769ec44
-size 2699
+oid sha256:14aedf982742401d9895309c8b9fbb49a1d44af09983e9581ca361c72402e7ca
+size 3137
