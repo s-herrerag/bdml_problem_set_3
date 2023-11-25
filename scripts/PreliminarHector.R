35ae2b31e9da5babe13ce66a3a45cfb31e1a5bc4
Héctor Segura <hd.segura@uniandes.edu.co>
2023-11-25 12:28:30 -0500

    Update PreliminarHector.R

--- scripts/PreliminarHector.R
+++ scripts/PreliminarHector.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ea044e2ffe69c46996e55ff92a6fe8c1fa2d513981083e0c380b0b9fef775b4
-size 3828
+oid sha256:105c70e0767ae109ad281f2c65075b4a079d6508f492a01a7d59cc86211e5d5d
+size 3874
